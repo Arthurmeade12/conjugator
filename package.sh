e0dc61f896719d6712fc8a6ae6fe0fc25e24b7fb
Arthurmeade12 <arthurmeade12@gmail.com>
2023-12-22 18:02:51 -0600

    Prepare for maven

--- package.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-TARGET="${TARGET:=.}"
-JARNAME='conjugator.jar'
-MAINCLASS='me.arthurmeade12.conjugator.main'
-COMPILEPATH='me/arthurmeade12/conjugator'
-WD="$(dirname "${0}")"
-javac -d "${TARGET}" "${WD}"/**/*.java # Will need to rewrite this if we have code somewhere other than src
-jar -c -f "${TARGET}/${JARNAME}" -e "${MAINCLASS}" "${TARGET}/${COMPILEPATH}"
